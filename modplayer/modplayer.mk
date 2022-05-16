7e40323ab19173f402569feab4b50890bc88608c
Didier Malenfant <coding@malenfant.net>
2022-05-16 15:01:31 +0200

    Added a guard against including the library multiple times
    Another extension could rely on the same library, this prevents the code from being included twice.

--- modplayer/modplayer.mk
+++ modplayer/modplayer.mk
@@ -25,17 +25,25 @@
 #  SOFTWARE.
 #
 
-# -- Find out more about where this file is relative to the Makefile including it
-RELATIVE_FILE_PATH := $(lastword $(MAKEFILE_LIST))
-RELATIVE_DIR := $(subst /$(notdir $(RELATIVE_FILE_PATH)),,$(RELATIVE_FILE_PATH))
-RELATIVE_PARENT_DIR := $(subst /$(notdir $(RELATIVE_DIR)),,$(RELATIVE_DIR))
-
-# -- Add us as an include search folder only if it's not already there
-uniq = $(if $1,$(firstword $1) $(call uniq,$(filter-out $(firstword $1),$1)))
-UINCDIR := $(call uniq, $(UINCDIR) $(RELATIVE_PARENT_DIR))
-
-# -- Add our source files
-SRC := $(SRC) \
-	   $(RELATIVE_DIR)/modplayer.c \
-	   $(RELATIVE_DIR)/platform.c \
-	   $(RELATIVE_DIR)/lmp/littlemodplayer.c
+ifndef NET_MALENFANT_MODPLAYER_PD
+	# -- Find out more about where this file is relative to the Makefile including it
+	RELATIVE_FILE_PATH := $(lastword $(MAKEFILE_LIST))
+	RELATIVE_DIR := $(subst /$(notdir $(RELATIVE_FILE_PATH)),,$(RELATIVE_FILE_PATH))
+	RELATIVE_PARENT_DIR := $(subst /$(notdir $(RELATIVE_DIR)),,$(RELATIVE_DIR))
+	
+	# -- Add us as an include search folder only if it's not already there
+	uniq = $(if $1,$(firstword $1) $(call uniq,$(filter-out $(firstword $1),$1)))
+	UINCDIR := $(call uniq, $(UINCDIR) $(RELATIVE_PARENT_DIR))
+	
+	# -- Add our source files
+	SRC := $(SRC) \
+	   	$(RELATIVE_DIR)/modplayer.c \
+	   	$(RELATIVE_DIR)/lmp/littlemodplayer.c
+	
+	# -- Add the utility library
+	include $(RELATIVE_DIR)/pdutility/pdutility.mk
+	
+	# -- We need to make sure this library won't be included twice in a project
+	# -- This could happen if the project uses tow libraries which use this one
+	NET_MALENFANT_MODPLAYER_PD := 1
+endif
